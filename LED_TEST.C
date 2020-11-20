a5a2b52015e999632bbea81918c8fff04ecab7f6
t <t>
2020-11-21 00:24:54 +0800

    8 led ws2812  drv ok;  voltage  2.3v-5v

--- LED_TEST.C
+++ LED_TEST.C
@@ -13,10 +13,11 @@
 //Variable definition
 //===========================================================
 
-#define  DIN PA2
-#define  led_r  0xa5
-#define  led_g  0x5a
-#define  led_b  0xa5
+#define  DIN PC4
+//unsigned char color[]={0x00,0x23,0x45,0x65,0x78,0x90,0x45,0x65,09};
+#define  led_r  0x77
+#define  led_g  0x77
+#define  led_b  0x77
 //===========================================================
 //Funtion name：sendbyte
 //parameters: dat
@@ -30,24 +31,25 @@ void sendbyte(unsigned char dat)
     if(dat&0x80)
     {
         DIN=1;
-        NOP();
-        NOP();
-       // NOP();
+        DIN=1;
+        DIN=1;
+        DIN=1;
+        DIN=1;
         DIN=0;
-        NOP();
+
     }
     else
     {
-       DIN=1;
-        //NOP();
+        DIN=1;
+        DIN=1;
+        DIN=1;
         DIN=0;
-        NOP();
-       // NOP();
-        //NOP();    
+        DIN=0;
+        DIN=0;
     }
     dat<<=1;
     }
-    
+    //DIN=1;
 }
 
 //===========================================================
@@ -85,10 +87,12 @@ void ws2812_rst(void)
 //===========================================================
 void  io_init(void)
 {
+   OSCCON = 0B01110001;//16M/2T
    ANSEL = 0b00000000;//关闭模拟端口
-   PORTA=0XFB;//PA2设置为输出
-   TRISA=0XFB;
-   OSCCON=0B01110111;//16M/2T
+   PORTA=0XFF;//PA0设置为输出
+   TRISA=0XFF;
+   PORTC=0XEF;//PA0设置为输出
+   TRISC=0XEF;
    MSCKCON = 0B00000000;//禁用LDO
    CMCON0 = 0B00000111; //关闭比较器，CxIN为数字IO口
 }
@@ -104,20 +108,49 @@ void interrupt ISR(void)
 //===========================================================
 //Funtion name：main
 //parameters：无
-//returned value：无  
+//returned value：无
 //===========================================================
 main()
 { 
-    unsigned char k;
+    unsigned char k,j,k,m;
+    unsigned char am[3]={0x80,0x00,0x00};
     io_init();
+    j=0;
     while(1)
     {
     ws2812_rst();
     for (k=8;k>0;k--)
     {
-    sendbyte(led_r);
-    sendbyte(led_g);
-    sendbyte(led_b);
+        if(k==j)
+        {
+          sendbyte(am[0]);
+          sendbyte(am[1]);
+          sendbyte(am[2]);
+        }
+        else
+        {
+           sendbyte(0x00);
+           sendbyte(0x00);
+           sendbyte(0x00);
+        }
+    }
+
+for(k=250;k>0;k--)
+{
+    delay100us();
+    delay100us();
+    delay100us();
+     delay100us();
+    delay100us();
+}
+  j++;
+    if(j==9)
+    {
+        j=1; 
+        m=am[0];
+        am[0]=am[1];
+        am[1]=am[2];
+        am[2]=m;     
     }
     }
 }
